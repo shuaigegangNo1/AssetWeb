e3970ffb299789908d23a0e274e7a4a7f8cb3286
黄雪雯 <huangxuewen@huangxuewen.local>
2018-04-29 15:15:05 +0800

    init commit

---
+++ src/app/common/entity/User.ts
@@ -0,0 +1,21 @@
+import {BaseModule} from './BaseModule';
+import {BaseCriteria} from './BaseCriteria';
+
+export class User extends BaseModule {
+  username?: string;
+  password?: string;
+  name?: string;
+  email?: string;
+  type: Type;
+}
+
+export enum Type {
+  admin,
+  Dadmin,
+  user
+}
+
+export class RMUserCriteria extends BaseCriteria {
+  name: string;
+  email: string;
+}
---
+++ src/app/common/shareModule.ts
@@ -0,0 +1,42 @@
+/**
+ * Created by huangxuewen on 2018/4/24.
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
+import {SimpleNotificationsModule} from "angular2-notifications";
+import {BsDropdownModule, ModalModule, TabsModule, SortableModule, PaginationModule} from 'ngx-bootstrap';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule,
+        // HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SimpleNotificationsModule.forRoot(),
+        ModalModule.forRoot(),
+        BsDropdownModule.forRoot(),
+        TabsModule.forRoot(),
+        SortableModule.forRoot(),
+        PaginationModule.forRoot(),
+    ],
+    declarations: [],
+    exports: [CommonModule,
+        FormsModule,
+        RouterModule,
+        // HttpModule,
+        SimpleNotificationsModule,
+        ReactiveFormsModule,
+        ModalModule,
+        BsDropdownModule,
+        TabsModule,
+        SortableModule,
+        PaginationModule
+        ]
+})
+export class SharedModules {
+}
